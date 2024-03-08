a1755e282c6aeb090d5d51b31874ab8241b99594
Dor Shir <117517821+Dorshir@users.noreply.github.com>
2024-03-08 11:09:55 +0200

    Update TimedSpawner.cs

--- Assets/Scripts/2-spawners/TimedSpawner.cs
+++ Assets/Scripts/2-spawners/TimedSpawner.cs
@@ -11,8 +11,6 @@ public class TimedSpawner : MonoBehaviour
     [SerializeField] Vector3 velocityOfSpawnedObject;
     [SerializeField] float secondsBetweenSpawns = 1f;
 
-    // OLD CODE using coroutines:
-    //
     void Start()
     {
         this.StartCoroutine(SpawnRoutine());
@@ -28,25 +26,4 @@ public class TimedSpawner : MonoBehaviour
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
     }
-
-    // NEW CODE using async-await - works on Windows but not on WebGL
-    //
-    // void Start() {
-    //     // SpawnRoutine();  // generates a warning
-    //     // SpawnRoutine().GetAwaiter();    
-    //     _ = SpawnRoutine();  // discard the awaiter - do not wait for this task to finish
-    //     Debug.Log("Start finished");
-    // }
-
-    // async Task SpawnRoutine() {
-    //     while (true) {
-    //         GameObject newObject = Instantiate(prefabToSpawn.gameObject, transform.position, Quaternion.identity);
-    //         newObject.GetComponent<Mover>().SetVelocity(velocityOfSpawnedObject);
-    //         await Task.Delay((int)(secondsBetweenSpawns*1000));
-    //         // To wait to the next frame, use:
-    //         // await Task.Yield();
-    //         // Credit: https://www.youtube.com/watch?v=WY-mk-ZGAq8
-    //     }
-    // }
-
 }
